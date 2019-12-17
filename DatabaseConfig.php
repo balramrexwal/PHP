073f6588a22277044c09acf195765e532ef398a4
balramrexwal <balramrexwal01@gmail.com>
2019-12-17 14:21:12 +0530

    Add files via upload

---
+++ DatabaseConfig.php
@@ -0,0 +1,15 @@
+<?php
+
+//Define your host here.
+$HostName = "192.168.0.102";
+
+//Define your database username here.
+$HostUser = "root";
+
+//Define your database password here.
+$HostPass = "toor";
+
+//Define your database name here.
+$DatabaseName = "new";
+
+?>
